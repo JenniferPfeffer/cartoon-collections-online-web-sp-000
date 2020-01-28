87a8650def781e65d9e68e3031ca423f6e44fed4
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-28 17:11:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,9 +15,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.find("cheese")
   if array.include?("cheese") == false 
     return nil 
+  else return array.
   end 
   
   cheese_types = ["cheddar", "gouda", "camembert"]
