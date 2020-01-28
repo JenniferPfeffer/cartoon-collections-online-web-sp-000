bb6d2e1c4166b5c98b8c153727ef126fc0fa8a2d
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-28 17:07:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each do |words| 
+  array.any? do |words| 
     if words.length > 4 
       return true  
     else return false
