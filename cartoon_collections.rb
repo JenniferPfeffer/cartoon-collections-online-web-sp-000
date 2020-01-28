a2a662cdc69ce19eb07d2271c7fbed4dffe45f6d
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-28 17:16:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,10 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese(array)
-  if array.include?("cheese") == false 
-    return nil 
-  else return array.find("cheese")
-  end 
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w[gouda cheddar camembert]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
 end
