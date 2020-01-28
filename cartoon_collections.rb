f33372df1b016080df1037717182d85bd2f68418
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-28 17:07:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,13 @@ def summon_captain_planet(array)
   array.map {|calls| calls.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each do |words| 
+    if words.length > 4 
+      return true  
+    else return false
+    end 
+  end
 end
 
 def find_the_cheese# code an argument here
